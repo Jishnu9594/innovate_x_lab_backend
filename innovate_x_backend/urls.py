0ef32df7672f3eb1b7fb9bde44beb2f6ed7901b6
Jishnu9594 <jishnuaswin025@gmail.com>
2025-01-04 11:43:22 +0530

    Initial commit

---
+++ innovate_x_backend/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("main/", include("local_apps.main.urls")),
+    path("innovatex/", include("local_apps.innovatex.urls")),
+    path("message_uitility/", include("local_apps.message_uitility.urls")),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
